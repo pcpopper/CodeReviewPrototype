f0893faa148bdac780bb0e8637593106d051fe05
Darren Eidson <dse2pd@mail.missouri.edu>
2015-08-17 14:54:56 -0500

    Updated menu html, menu.js, and css

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,29 +1,86 @@
 var SideMenu = function (pageOptions) {
 
+    this.itemDivs = $("#menu #wrapper div");
+    this.menuCollapseDiv = $('#menu #button');
+    this.menuDiv = $('#menu');
+    this.pageWrapper = $('#pageWrapper');
+
     this.options = {
-        "itemDivs"      : $("#menu #wrapper div"),
         "menuOut"       : false,
-        "menuCurrent"   : 'Home'
+        "menuCurrent"   : 'Home',
+        "newMargin"     : 0,
+        "origMargin"    : 0,
+        "newPadding"    : 0,
+        "oldPadding"    : 0
     };
 
     this.initialize = function (pageOptions) {
+        var _self = this;
         this.options = $.extend(this.options, JSON.parse(pageOptions));
         this.options.menuOut = (this.options.menuOut == "1");
+        this.options.newMargin = this.menuDiv.width() + parseInt(this.menuDiv.css('margin-left').replace('px', ''));
+        this.options.origMargin = - parseInt(this.menuDiv.css('margin-left').replace('px', ''));
+        this.options.oldPadding = parseInt(this.pageWrapper.css('padding-left').replace('px', ''));
+        this.options.newPadding = - (this.menuDiv.width() - 16 - parseInt(this.pageWrapper.css('padding-left').replace('px', '')));
+
+        this.toggleMenu();
+
+        setTimeout(function () {
+            _self.menuDiv.addClass('transition');
+            _self.pageWrapper.addClass('transition');
+        },500);
 
         this.getCurrent();
+        this.bindElements();
     };
 
     this.getCurrent = function () {
         var _self = this;
-        this.options.itemDivs.each(function (idx, div) {
+        this.itemDivs.each(function (idx, div) {
             if (div.id.toLowerCase() == 'menu'+_self.options.menuCurrent.toLowerCase()) {
                 $(div).addClass('current');
             }
         });
     };
 
-    this.moveOut = function () {};
-    this.moveIn = function () {};
+    this.bindElements = function() {
+        this.menuCollapseDiv.bind("click", {
+            "newMargin"         : this.options.newMargin,
+            "origMargin"        : this.options.origMargin,
+            "newPadding"        : this.options.newPadding,
+            "oldPadding"        : this.options.oldPadding,
+            "menuDiv"           : this.menuDiv,
+            "menuCollapseDiv"   : this.menuCollapseDiv,
+            "pageWrapper"       : this.pageWrapper
+        }, this.toggleMenu);
+    };
+
+    this.toggleMenu = function (event) {
+        if (event) {
+            this.options = {
+                "newMargin"     : event.data.newMargin,
+                "origMargin"    : event.data.origMargin,
+                "newPadding"    : event.data.newPadding,
+                "oldPadding"    : event.data.oldPadding
+            };
+            this.pageWrapper = $(event.data.pageWrapper);
+            this.menuDiv = $(event.data.menuDiv);
+            this.options.menuOut = this.menuDiv.data('collapsed');
+            this.menuCollapseDiv = $(event.data.menuCollapseDiv);
+        }
+
+        if (this.options.menuOut) {
+            this.menuDiv.css('margin-left', - this.options.origMargin);
+            this.menuCollapseDiv.html('<');
+            this.menuDiv.data('collapsed', 0);
+            this.pageWrapper.css('padding-left', this.options.oldPadding)
+        } else {
+            this.menuDiv.css('margin-left', - this.options.newMargin);
+            this.menuCollapseDiv.html('>');
+            this.menuDiv.data('collapsed', 1);
+            this.pageWrapper.css('padding-left', this.options.newPadding)
+        }
+    };
 
     return this.initialize(pageOptions);
 };
